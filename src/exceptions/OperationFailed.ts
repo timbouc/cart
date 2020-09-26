1e7e4d7b8c5544b910efb23ccb2c7df3ab3560f7
Daniel Smit <dcsmit7@gmail.com>
2020-09-26 23:27:02 +0930

    Implement content function

--- src/exceptions/OperationFailed.ts
+++ src/exceptions/OperationFailed.ts
@@ -13,10 +13,16 @@ export class OperationFailed extends Error {
 	public static cartUpdate(msg: string|null = null): OperationFailed {
 		return new this('Failed to update cart' + msg? `: ${msg}`:'');
 	}
+	public static compute(msg: string|null = null): OperationFailed {
+		return new this('Failed to compute for cart' + msg? `: ${msg}`:'');
+	}
 	public static condition(msg: string|null = null): OperationFailed {
 		return new this('Failed to get condition' + msg? `: ${msg}`:'');
 	}
 	public static getItem(msg: string|null = null): OperationFailed {
 		return new this('Failed to get item' + msg? `: ${msg}`:'');
+	}
+	public static parseString(msg: string|null = null): OperationFailed {
+		return new this('Failed to parse string' + msg? `: ${msg}`:'');
 	}
 }
